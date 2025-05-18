2844528d600aac858769c84ddad953bd24f58f4e
gabrielhenriqueconstantino <gabrielconstantinogh@gmail.com>
2025-05-18 16:29:23 -0300

    Feito o Header da página

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './styles/Global.css';
+import './styles/global.css';
+import './styles/App.css';
+
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Bem-vindo ao meu site!</h1>
-      <p>Agora o React está rodando.</p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,58 @@
+// Componente do cabeçalho com navegação e menu hambúrguer para telas pequenas
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import '../styles/Header.css';
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="header">
+      <nav className="nav">
+        <div className="logo">
+        <img src="http://votorantim.citymais.com.br/wp-content/uploads/2020/07/logo-city3.png" alt="Logo da CITY+"></img>
+          <Link to="/"></Link>
+        </div>
+          
+        <div className="hamburger" onClick={toggleMenu}>
+          <span className={`bar bar1 ${menuOpen ? 'open' : ''}`}></span>
+          <span className={`bar bar2 ${menuOpen ? 'open' : ''}`}></span>
+          <span className={`bar bar3 ${menuOpen ? 'open' : ''}`}></span>
+        </div>
+
+        <ul className={`nav-links ${menuOpen ? 'active' : ''}`}>
+          <li>
+            <NavLink to="/" end onClick={() => setMenuOpen(false)}
+              className={({ isActive }) => (isActive ? 'active' : '')}>
+              Horários
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects" onClick={() => setMenuOpen(false)}
+              className={({ isActive }) => (isActive ? 'active' : '')}>
+              Intinerários
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/blog" onClick={() => setMenuOpen(false)}
+              className={({ isActive }) => (isActive ? 'active' : '')}>
+              Contato
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" onClick={() => setMenuOpen(false)}
+              className={({ isActive }) => (isActive ? 'active' : '')}>
+              Cadastro
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
